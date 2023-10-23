6a6ed490e3322b154cdb5bf8a6b7e8ceb36d2dc1
Programmer-Timmy <122127801+programmer-timmy@users.noreply.github.com>
2023-10-24 00:58:06 +0200

    Added SCSS updated some settings
    Signed-off-by: Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>

--- private/config/settings.php
+++ private/config/settings.php
@@ -17,15 +17,8 @@ $site = [
     ];
 
 /**
- * Page titles wil automatically be defined by url.
- * You can change a page title by adding this:
- * '<file name here>' => '<Your custom Title>',
- *
- * optional:
- *  You can use @siteName to add your site name to it!
- * it wil look like this:
- *  '<file name here>' => '<Your custom Title> - ' . $site['siteName'],
- **/
+ * Page Title Settings
+ */
 $url = $_SERVER['REQUEST_URI'];
 if ($url == '/') {
     $url = '/home';
