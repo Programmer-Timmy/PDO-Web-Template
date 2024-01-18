ad94fa2e3ab43c41b66c38d90a10950710c56653
Programmer-Timmy <tim.vanderkloet@gmail.com>
2024-01-18 12:22:53 +0100

    updated the bool and added some clearity

--- private/views/pages/home.php
+++ private/views/pages/home.php
@@ -1,1 +1,4 @@
-<?= GlobalUtility::createTable(Database::getAll('projects'), ['name', 'description', 'id', 'date'], false, true) ?>
+
+<div class="container">
+<?= GlobalUtility::createTable(Database::getAll('projects'), ['name', 'date', 'removed'], [['class' => 'btn btn-danger', 'action'=> '?delete=', 'label' => 'X'],['class' => 'btn btn-primary', 'action'=> 'edit_project?id=', 'label' => '...']], true) ?>
+</div>
